361056c54c90be1dd6986093d85b7b26bc09da5d
RamaTeja99 <144083564+RamaTeja99@users.noreply.github.com>
2023-11-07 17:16:56 +0530

    Update front-end

--- front-end/src/components/SurveySubmissionPage.js
+++ front-end/src/components/SurveySubmissionPage.js
@@ -1,22 +1,22 @@
-// SurveySubmissionPage.js 
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams ,Link} from 'react-router-dom';
-import './SurveySubmissionPage.css'
+import { useParams, Link } from 'react-router-dom';
+import './SurveySubmissionPage.css';
 
 const SurveySubmissionPage = () => {
-  const { formId } = useParams(); // Extract the formId from the URL parameters
+  const { formId } = useParams();
   const [survey, setSurvey] = useState(null);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState(''); // Added phoneNumber state
+  const [responses, setResponses] = useState({});
 
   useEffect(() => {
-    // Fetch survey data when the component mounts
     fetchSurveyData();
   }, []);
 
   const fetchSurveyData = async () => {
     try {
-      // Fetch the survey data based on the formId
       const response = await axios.get(`http://localhost:8080/form/getSurveyData/${formId}`);
       const surveyData = response.data.data;
       setSurvey(surveyData);
@@ -25,87 +25,84 @@ const SurveySubmissionPage = () => {
     }
   };
 
-  const SubmitSurveyForm = ({ formId, questions }) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [responses, setResponses] = useState(Array(questions.length).fill(''));
+  const handleResponseChange = (questionId, event) => {
+    const updatedResponses = { ...responses, [questionId]: event.target.value };
+    setResponses(updatedResponses);
+  };
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const response = {
-          formId,
-          name,
-          email,
-          answers: responses,
-        };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = {
+        formId,
+        name,
+        email,
+        phoneNumber, // Include phoneNumber in the response
+        answers: Object.values(responses), // Extract values from the responses object
+      };
 
-        // Send the response data to the server
-        const submitResponse = await axios.post('http://localhost:8080/form/submitResponse', response);
+      // Send the response data to the server
+      const submitResponse = await axios.post('http://localhost:8080/form/submitResponse', response, {
+        headers: {
+          'Content-Type': 'application/json', // Set the content type header
+        },
+      });
 
-        // Handle success or error as needed
-        console.log('Response submitted:', submitResponse.data);
-      } catch (error) {
-        console.error('Error submitting response:', error);
-      }
-    };
-
-    const handleResponseChange = (questionIndex, event) => {
-      const updatedResponses = [...responses];
-      updatedResponses[questionIndex] = event.target.value;
-      setResponses(updatedResponses);
-    };
-
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>Name:</label>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-
-          {questions.map((question, index) => (
-            <div key={index}>
-              <label>Question {index + 1}:</label>
-              <p>{question.text}</p>
-              {question.type === 'text' ? (
-                <input
-                  type="text"
-                  value={responses[index] || ''}
-                  onChange={(event) => handleResponseChange(index, event)}
-                />
-              ) : (
-                <select
-                  value={responses[index] || ''}
-                  onChange={(event) => handleResponseChange(index, event)}
-                >
-                  <option value="">Select an option</option>
-                  {question.options.map((option, optionIndex) => (
-                    <option key={optionIndex} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-              )}
-            </div>
-          ))}
-
-          <button type="submit">Submit Response</button>
-        </form>
-      </div>
-    );
+      // Handle success or error as needed
+      console.log('Response submitted:', submitResponse.data);
+    } catch (error) {
+      console.error('Error submitting response:', error);
+    }
   };
 
   return (
-    <div className='Submission'>
+    <div className="Submission">
       <h2>Survey Submission Page for Form ID: {formId}</h2>
-      <Link to="/dashboard" className="back-button">Back to Dashboard</Link> 
+      <Link to="/dashboard" className="back-button">
+        Back to Dashboard
+      </Link>
       {survey ? (
         <div>
           <h3>{survey.title}</h3>
           <p>{survey.description}</p>
-          <SubmitSurveyForm formId={formId} questions={survey.questions} />
+          <form onSubmit={handleSubmit}>
+            <label>Name:</label>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+            <label>Email:</label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+            <label>Phone Number:</label>
+            <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+
+            {survey.questions.map((question) => (
+              <div key={question._id}>
+                <label>Question:</label>
+                <p>{question.text}</p>
+                {question.type === 'text' ? (
+                  <input
+                    type="text"
+                    value={responses[question._id] || ''}
+                    onChange={(event) => handleResponseChange(question._id, event)}
+                  />
+                ) : (
+                  <select
+                    value={responses[question._id] || ''}
+                    onChange={(event) => handleResponseChange(question._id, event)}
+                  >
+                    <option value="">Select an option</option>
+                    {question.options.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+                )}
+              </div>
+            ))}
+
+            <button type="submit">Submit Response</button>
+          </form>
         </div>
       ) : (
         <p>Loading survey data...</p>
